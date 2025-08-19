420bacfebf01dc24adf7d0187d8c8c1f55290eb7
Paulo Junio <126300867+Paul1nhu@users.noreply.github.com>
2025-08-19 16:25:58 -0300

    Testando em horas

--- aula24/index.js
+++ aula24/index.js
@@ -1,5 +1,9 @@
-const hora = 10
+const hora = 12
 
-if (hora < 12) {
+if (hora >= 5 && hora <= 12) {
     console.log(`Bom dia!`)
+} else if (hora >= 13 && hora <= 17 ) {
+    console.log(`Boa tarde!`)
+} else if (hora >= 18 && hora <= 23) {
+    console.log(`Boa noite!`)
 }

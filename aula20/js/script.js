6958d179b681e99097c22ca44c87d0b598780499
Paulo Junio <126300867+Paul1nhu@users.noreply.github.com>
2025-08-19 00:23:50 -0300

    Finalizado o exercicio com sucesso

--- aula20/js/script.js
+++ aula20/js/script.js
@@ -1,24 +1,34 @@
 function escopo () {
  const form = document.querySelector(`.form`)
 
- function resultado () {
-    const resultado = document.querySelector(`.resultado`)
-
-
- };
-
  const pessoas = [];
 
-function receberevento (evento) {
-    evento.preventDefault()
-    const nome = form.querySelector(`.nm`)
-    const sobreNome = form.querySelector(`.sm`)
-    const idade = form.querySelector(`.num`)
-    const altura = form.querySelector(`.alt`)
-    const peso = form.querySelector(`.kg`)
-    
-    console.log(nome.value, sobreNome.value, peso.value, altura.value, idade.value)
+    function receberevento (evento) {
+        evento.preventDefault()
+        const nome = form.querySelector(`.nm`)
+        const sobreNome = form.querySelector(`.sm`)
+        const idade = form.querySelector(`.num`)
+        const altura = form.querySelector(`.alt`)
+        const peso = form.querySelector(`.kg`)
+
+            const pessoa = {
+            nome:nome.value,
+            sobreNome:sobreNome.value,
+            idade:idade.value,
+            altura:altura.value,
+            peso:peso.value
 };
+
+    function resultado () {
+        const resultado = document.querySelector(`.resultado`)
+        resultado.innerHTML += (`<br> Nome: ${nome.value}, Sobrenome: ${sobreNome.value}, Idade ${idade.value}, Altura: ${altura.value} e peso: ${peso.value}.`)
+    };
+
+    pessoas.push(pessoa)
+    console.log(pessoas)
+    resultado()
+};
+
 
 form.addEventListener('submit', receberevento)
 };

51ffffd1be2a15aa3c0eed2343d0855a62643074
Paulo Junio <126300867+Paul1nhu@users.noreply.github.com>
2025-08-20 23:25:46 -0300

    Finaliado!

--- modelo/assets/js/indexatvd.js
+++ modelo/assets/js/indexatvd.js
@@ -9,25 +9,30 @@ function escopo(){
         function calculo () {
             const resultadoPeso = peso.value
             const resultadoAltura = altura.value
-
             const IMC = resultadoPeso / (resultadoAltura * resultadoAltura)
-            console.log(IMC)
-            return
+            let resultadoFinal = null
+                if(IMC < 18.5) {
+                resultadoFinal = 'Abaixo do peso'
+                } else if (IMC >= 18.5 && IMC <= 24.9) {
+                resultadoFinal = `Peso normal`
+                } else if (IMC >= 25 && IMC <= 29.9) {
+                resultadoFinal = `Sobrepeso`
+                } else if (IMC >= 30 && IMC <= 34.9) {
+                resultadoFinal = `Obesidade grau 1`
+                } else if (IMC >= 35 && IMC <= 39.9) {
+                resultadoFinal = `Obesidade grau 2`
+                } else {
+                resultadoFinal = `Obesidade grau 3`
+                }
+
+                    function resultado () {
+                    const resultado = document.querySelector(`.resultado2`)
+                    resultado.innerHTML = (`Voce tem ${peso.value}kg, tem ${altura.value} de altura e tem ${IMC.toFixed(2)} de IMC ou seja voce eh considerado: ${resultadoFinal}.`)
+                    }
+            resultado()
         }
         calculo()
-
-        function resultado () {
-            const resultado = document.querySelector(`.resultado2`)
-            resultado.innerHTML = (`Voce tem ${peso.value}kg, ${altura.value} de altura e esta: `)
-        }
-        resultado()
-
-        function IMC () {
-            // continuar fazendo o IF, else IF e ELSE (Fazer o seguinte, se o resultado do IMC for maior q x valor vai ser printado o sobrepeso, se for maior q y valor sera printado o peso normal e por ai vai... (: )
-        }
     };
-
 form.addEventListener('submit', principal)
 };
-
 escopo()

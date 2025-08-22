f2844ece63601d0ba050cb23bdab3f31411574b1
Paulo Junio <126300867+Paul1nhu@users.noreply.github.com>
2025-08-21 23:30:24 -0300

    Finalizei o exercicio

--- modelo/assets/js/indexatvd.js
+++ modelo/assets/js/indexatvd.js
@@ -4,40 +4,59 @@ function escopo() {
         e.preventDefault()
         principal()
     })
-
     function principal () {
         const peso = form.querySelector(`.peso`)
         const altura = form.querySelector(`.altura`)
-        const resultadoPeso = peso.value
-        const resultadoAltura = altura.value
+        const resultadoPeso = Number(peso.value)
+        const resultadoAltura = Number(altura.value)
+        const IMCR = calculoIMC(resultadoPeso, resultadoAltura)
+        const nivelIMC = nivel_imc(IMCR);
+        const mensagem = `Seu IMC eh ${IMCR} (${nivelIMC}).`
+
+        resultado(mensagem, true)
+            if (!resultadoPeso) {
+            resultado(`Peso invalido!`, false)
+            return;
+            }
+            if (!resultadoAltura) {
+            resultado(`Altura invalida!`, false)
+            return;
+            }
+        function calculoIMC(resultadoPeso, resultadoAltura ) {
+            const IMC = resultadoPeso / resultadoAltura ** 2;
+            return IMC.toFixed(2);
+        };
+
+        function nivel_imc (calculoIMC) {
+            listaIMC = [`Abaixo do peso`,`Peso normal`, `Sobrepeso`, `Obesidade grau 1`, `Obesidade grau 2`, `Obesidade grau 3`];
+            if (calculoIMC >= 39.9) return listaIMC[5];
+            if (calculoIMC >= 34.9) return listaIMC[4];
+            if (calculoIMC >= 29.9) return listaIMC[3];
+            if (calculoIMC >= 24.9) return listaIMC[2];
+            if (calculoIMC >= 18.5) return listaIMC[1];
+            if (calculoIMC < 18.5) return listaIMC[0];
             
-            let resultadoFinal = null
-                if (resultadoPeso == "" || resultadoAltura == "") {
-                    const msgErro = `Favor coloque uma altura e peso validos.`
-                    resultado(msgErro)
-                    return
-                }
-                const IMC = resultadoPeso / (resultadoAltura * resultadoAltura)
-                if(IMC < 18.5) {
-                resultadoFinal = 'Abaixo do peso'
-                } else if (IMC >= 18.5 && IMC <= 24.9) {
-                resultadoFinal = `Peso normal`
-                } else if (IMC >= 25 && IMC <= 29.9) {
-                resultadoFinal = `Sobrepeso`
-                } else if (IMC >= 30 && IMC <= 34.9) {
-                resultadoFinal = `Obesidade grau 1`
-                } else if (IMC >= 35 && IMC <= 39.9) {
-                resultadoFinal = `Obesidade grau 2`
-                } else {
-                resultadoFinal = `Obesidade grau 3`
-                }
-                const msgSucesso = `Voce tem ${peso.value}kg, tem ${altura.value} de altura e tem ${IMC.toFixed(2)} de IMC ou seja voce eh considerado: ${resultadoFinal}.`;
-                resultado(msgSucesso)
-    };
+};        
+};
+        function criarP () {
+            const p = document.createElement('p');
+            return p;
+}
+
         function resultado(mensagem, validar) {
             const divResultado = document.querySelector('.resultado2');
-            divResultado.innerHTML = mensagem;
-        }
+            divResultado.innerHTML = '';
+            const p = criarP();
+            
+            if (validar) {
+                p.classList.add('correto');
+            } else {
+              p.classList.add('erro')
+            }
+
+            p.innerHTML = mensagem;
+            divResultado.appendChild(p)
+}
 };
 escopo()
 

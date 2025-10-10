c2be01b8c73fd7f5c968197cc3acf776bfe511fa
Paulo Junio <paulohenriquejunio@hotmail.com>
2025-10-10 17:39:46 -0300

    Atividade ePaisagem

--- atvdbestas/index3.js
+++ atvdbestas/index3.js
@@ -1,13 +1,20 @@
-function ePaisagem (largura, altura) {
-    if (largura > altura) {
-        return(`Esta no modo paisagem`)
-    } else {
-        return(`Esta no modo retrato`)
-    };
-};
+// function ePaisagem (largura, altura) {
+//     if (largura > altura) {
+//         return(`Esta no modo paisagem`)
+//     } else {
+//         return(`Esta no modo retrato`)
+//     };
+// };
+
+// const largura = 1920;
+// const altura = 1080;
+
+// const resultado = ePaisagem(largura, altura);
+// console.log(resultado);
+
+const paisagem = (largura, altura) => largura > altura ? 'Ta no modo paisagem' : 'Ta no modo retrato';
 
 const largura = 1920;
-const altura = 1080;
+const altura = 1920;
 
-const resultado = ePaisagem(largura, altura);
-console.log(resultado);
+console.log(paisagem(largura,altura));
